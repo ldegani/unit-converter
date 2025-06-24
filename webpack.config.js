3d7446f91d2c745062a84d2e4b0719ac287c620f
Lucas Degani <57118989+ldegani@users.noreply.github.com>
2025-06-24 15:36:19 -0300

    Refactor HTML structure and start CSS styling
    Improved the HTML structure for better semantics and started applying
    CSS to support the updated layout and styling. Both changes work
    together to improve the structure and visual presentation of the project

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,8 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
